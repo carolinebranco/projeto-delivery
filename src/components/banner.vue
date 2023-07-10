d84160f1a9cb85e0cc0122073b9c54f8de941359
caroline branco <101421521+carolinebranco@users.noreply.github.com>
2023-07-10 13:40:41 -0300

    Update banner.vue

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -27,5 +27,11 @@
     font-size: 60px;
     background-color: #222;
     padding: 20px 40px;
+    <template>
+  <div id="main-banner">
+    <h1>Make Your Burger</h1>
+    <h1>escolha o molho<h1>
+  </div>
+</template>
 }
 </style>
